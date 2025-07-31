0b0ec16b560b176e842b879aca35daf245eef560
anushree199 <60727856+anushree199@users.noreply.github.com>
2025-07-31 23:54:26 +0530

    Add files via upload

---
+++ resources/ecs/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {}
+variable "task_family" {}
+variable "cpu" {}
+variable "memory" {}
+variable "image" {}
+variable "container_port" {}
+variable "execution_role_arn" {}
+variable "task_role_arn" {}
+variable "log_group_name" {}
+variable "region" {}
+variable "service_name" {}
+variable "desired_count" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "security_group_ids" {
+  type = list(string)
+}
+//variable "target_group_arn" {}
