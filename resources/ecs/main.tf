52f524649de9c8d082e32a57f75ae59b66ea9dc9
anushree199 <60727856+anushree199@users.noreply.github.com>
2025-07-31 23:53:32 +0530

    Create main.tf

---
+++ resources/ecs/main.tf
@@ -0,0 +1,54 @@
+resource "aws_ecs_cluster" "ecs" {
+  name = var.cluster_name
+}
+
+resource "aws_ecs_task_definition" "ecs_task" {
+  family                   = var.task_family
+  cpu                      = var.cpu
+  memory                   = var.memory
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = var.execution_role_arn
+  task_role_arn            = var.task_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.task_family
+      image     = var.image
+      portMappings = [{
+        containerPort = var.container_port
+        hostPort      = var.container_port
+      }]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = var.log_group_name
+          awslogs-region        = var.region
+          awslogs-stream-prefix = var.service_name
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.ecs.id
+  task_definition = aws_ecs_task_definition.ecs_task.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnet_ids
+    security_groups = var.security_group_ids
+    assign_public_ip = false
+  }
+
+  # load_balancer {
+  #   target_group_arn = var.target_group_arn
+  #   container_name   = var.task_family
+  #   container_port   = var.container_port
+  # }
+
+  depends_on = [aws_ecs_task_definition.ecs_task]
+}
