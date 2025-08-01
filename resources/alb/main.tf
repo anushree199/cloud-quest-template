810cb5492112b9025ea32f1edc33f0f8672362db
anushree199 <60727856+anushree199@users.noreply.github.com>
2025-08-02 01:17:28 +0530

    Update main.tf

--- resources/alb/main.tf
+++ resources/alb/main.tf
@@ -54,3 +54,21 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
+
+# resource "aws_acm_certificate" "self_signed" {
+#   private_key       = file("${path.module}/../../cert/key.pem")
+#   certificate_body  = file("${path.module}/../../cert/cert.pem")
+# }
+
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = aws_lb.app_alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = aws_acm_certificate.self_signed.arn
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.app_tg.arn
+#   }
+# }
